e4e41b59dbc0f033fe1972117752626957921e57
Niel Lebeck <niel.lebeck@gmail.com>
2020-03-25 11:09:25 -0700

    Add canonical file name generation

--- upload-videos.py
+++ upload-videos.py
@@ -1,3 +1,7 @@
+# My main resource in writing this script was the Python documentation.
+# Here are some other links that I found useful:
+# https://stackoverflow.com/a/4172465
+
 import os
 from pathlib import Path
 import time
@@ -9,6 +13,17 @@ def get_size_megabytes(path):
 
 def get_modification_time(path):
     return time.gmtime(os.path.getmtime(path))
+
+def struct_time_to_string(time):
+    return repr(time.tm_year) \
+        + '-' + repr(time.tm_mon) \
+        + '-' + repr(time.tm_mday) \
+        + '-' + repr(time.tm_hour) \
+        + '-' + repr(time.tm_min) \
+        + '-' + repr(time.tm_sec)
+
+def get_canonical_name(path):
+    return user_name + '-' + struct_time_to_string(get_modification_time(path)) + path.suffix
 
 def read_config_params():
     config_dict = dict()
@@ -23,9 +38,10 @@ def read_config_params():
 
 config_params = read_config_params()
 video_dir = config_params['VIDEO_DIR']
+user_name = config_params['USER_NAME']
 
 p = Path(video_dir)
 
 for x in p.iterdir():
     print(get_size_megabytes(x))
-    print(get_modification_time(x))
+    print(get_canonical_name(x))
